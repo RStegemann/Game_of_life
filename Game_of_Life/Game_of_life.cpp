b189b80d67074abe5039bc351b892be5fb330438
Unknown <robinstegemann@hotmail.de>
2018-12-12 23:37:04 +0100

    Actually working stuff

--- Game_of_Life/Game_of_life.cpp
+++ Game_of_Life/Game_of_life.cpp
@@ -4,84 +4,69 @@
 #include <thread>
 #include <limits>
 #include <ctime>
-#include "mpi.h"
-
-
 
 using namespace std;
 using namespace chrono;
 
-void update_array(bool** old_array, bool** new_field);
-bool check_cell(bool** old_array, int row, int col);
-
-int max_cols;
-int max_rows;
-int field_size;
+void update_array(bool** old_array, bool** new_field, const int max_cols, const int max_rows, const int field_size, const Uint32* chunk_true, const Uint32* chunk_false);
+bool check_cell(bool** old_array, int row, int col, const int max_cols, const int max_rows);
 
 SDL_Texture *texture;
-Uint32 color_true;
-Uint32 color_false;
 
 int main(int argc, char *argv[]) {
 
 	srand(time(0));
 
-	field_size = 5;
+	const int field_size = 1;
 
-	max_rows = 100;
-	max_cols = 100; 
-	
-	color_false = 0;
-	color_true = numeric_limits<Uint32>::max();
+	const int max_rows = 1000;
+	const int max_cols = 1080;
+
+	const Uint32 color_false = 0;
+	const Uint32 color_true = numeric_limits<Uint32>::max();
 
 	int width = max_cols * field_size;
 	int height = max_rows * field_size;
+	
+	Uint32* chunk_true = new Uint32[field_size * field_size];
+	Uint32* chunk_false = new Uint32[field_size * field_size];
 
-	bool **old = new bool*[max_rows];
-	for (int i = 0; i < max_cols; ++i) {
-		old[i] = new bool[max_cols];
+	for (int i = 0; i < field_size * field_size; i++) {
+		chunk_true[i] = color_true;
+		chunk_false[i] = color_false;
 	}
 
+	bool **old = new bool*[max_rows];
 	bool **new_field = new bool*[max_rows];
+
 	for (int i = 0; i < max_cols; ++i) {
+		old[i] = new bool[max_cols];
 		new_field[i] = new bool[max_cols];
 	}
 
 	for (int col = 0; col < max_cols; ++col) {
 		for (int row = 0; row < max_rows; ++row) {
-			old[row][col] = rand() % 2;
+			old[row][col] = rand() % 10 == 0;
 		}
 	}
 
-	//this_thread::sleep_for(milliseconds(10000));
+	SDL_Init(SDL_INIT_EVERYTHING);
 
-	SDL_Window *screen = SDL_CreateWindow("Test", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, SDL_WINDOW_SHOWN);
-	SDL_Event event;
+	SDL_Window *screen = SDL_CreateWindow("Game of Life", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, SDL_WINDOW_SHOWN);
 	SDL_Renderer *renderer = SDL_CreateRenderer(screen, -1, 0);
+	SDL_Event event;
 	texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, width, height);
-
-	/*int numtasks, rank;
-
-	int rc = MPI_Init(NULL, NULL);
-	if (rc != MPI_SUCCESS) {
-		printf("Error starting MPI program. Terminating.\n");
-		MPI_Abort(MPI_COMM_WORLD, rc);
-	}
-	MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-
-	cout << "Greetings from Process: " << rank << "\n";
-	MPI_Finalize();*/
-
+	
 	for (;;) {
-		
+
+
 		while (SDL_PollEvent(&event)) {
 			if (event.type == SDL_QUIT) {
 				goto exit_app;
 			}
 		}
 
-		update_array(old, new_field);
+		update_array(old, new_field, max_cols, max_rows, field_size, chunk_true, chunk_false);
 		old = new_field;
 
 		//Redraw window
@@ -90,46 +75,35 @@ int main(int argc, char *argv[]) {
 		SDL_RenderPresent(renderer);
 	}
 
-	exit_app:
-		return 0;
+exit_app:
+	return 0;
 }
 
-void update_array(bool** old_array, bool** new_field) {
-
-	Uint32 *pixels = new Uint32[max_cols * field_size];
+void update_array(bool** old_array, bool** new_field, const int max_cols, const int max_rows, const int field_size, const Uint32* chunk_true, const Uint32* chunk_false) {
 
 	SDL_Rect rect;
 	rect.x = 0;
 	rect.y = 0;
-	rect.h = 1;
-	rect.w = field_size * max_cols;
+	rect.h = field_size;
+	rect.w = field_size;
 
 	for (int row = 0; row < max_rows; ++row) {
 		for (int col = 0; col < max_cols; ++col) {
-			cout << col  << "," << row << endl;
-			new_field[row][col] = check_cell(old_array, row, col);
-
-			int offset = col * field_size;
+			new_field[row][col] = check_cell(old_array, row, col, max_rows, max_cols);
 			if (new_field[row][col]) {
-				for (int i = 0; i < field_size; ++i) {
-					pixels[offset + i] = color_true;
-				}
+				SDL_UpdateTexture(texture, &rect, chunk_true, field_size * sizeof(Uint32));
 			}
 			else {
-				for (int i = 0; i < field_size; ++i) {
-					pixels[offset + i] = color_false;
-				}
+				SDL_UpdateTexture(texture, &rect, chunk_false, field_size * sizeof(Uint32));
 			}
+			rect.x = rect.x + field_size;
 		}
-
-		for (int i = 0; i < field_size; ++i) {
-			SDL_UpdateTexture(texture, &rect, pixels, (field_size * max_cols) * sizeof(Uint32));
-			++rect.y;
-		}
+		rect.y = rect.y + field_size;
+		rect.x = 0;
 	}
 }
 
-bool check_cell(bool** old_array, int row, int col) {
+bool check_cell(bool** old_array, int row, int col, const int max_rows, const int max_cols) {
 
 	int alive_count = 0;
 
@@ -196,7 +170,7 @@ bool check_cell(bool** old_array, int row, int col) {
 			bottom_right = old_array[0][col + 1];
 		}
 		else if (col == 0) {
-			upper_left = old_array[row - 1][(max_cols -1)];
+			upper_left = old_array[row - 1][(max_cols - 1)];
 			upper_center = old_array[row - 1][col];
 			upper_right = old_array[row - 1][col + 1];
 			middle_left = old_array[row][(max_cols - 1)];
@@ -265,7 +239,7 @@ bool check_cell(bool** old_array, int row, int col) {
 	if (alive_count < 2) {
 		return false;
 	}
-	if (old_array[row][col] && alive_count <= 3) {
+	if (old_array[row][col] && (alive_count == 2 || alive_count == 3)) {
 		return true;
 	}
 	if (alive_count > 3) {
